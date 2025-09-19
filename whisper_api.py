04b3b0feb84a605d1693a6a656b048825838ceac
Katecito <loko2003elcrack@gmail.com>
2025-09-18 22:14:12 -0500

    Integracion ala api sobre IA (Retornar texto segun la imagen Base64)

--- whisper_api.py
+++ whisper_api.py
@@ -1,3 +1,4 @@
+# WhisperAPI
 from fastapi import FastAPI
 from pydantic import BaseModel
 import base64
@@ -5,6 +6,12 @@ import whisper
 import tempfile
 import sys
 import os
+# Blip
+import base64
+from io import BytesIO
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from PIL import Image
+import traductorFuncional as trx
 
 # Forzar UTF-8
 sys.stdout.reconfigure(encoding='utf-8')
@@ -37,11 +44,32 @@ def transcribe_audio(req: AudioRequest):
     return {"text": result["text"]}
 
 
+def image_from_base64(base64_string):
+    image_data = base64.b64decode(base64_string)
+    return Image.open(BytesIO(image_data))
+
+
+# Cargar BLIP
+processor = BlipProcessor.from_pretrained(
+    "Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained(
+    "Salesforce/blip-image-captioning-base")
+
+
 class ImagenRequest(BaseModel):
     imagen_base64: str
 
 
 @app.post("/getTextImg")
 def transcribe_Imagen(req: ImagenRequest):
-    # Decodificar base64 a bytes
-    Img_bytes = base64.b64decode(req.imagen_base64)
+
+    # Convertir base64 a imagen
+    image = image_from_base64(req.imagen_base64)
+# Procesar con BLIP
+    inputs = processor(image, return_tensors="pt")
+    out = model.generate(**inputs)
+    caption = processor.decode(out[0], skip_special_tokens=True)
+
+# Traducir al español
+    caption = trx.traduccionSpanish(caption)
+    return {"text": caption}
